95b0310e50684beb5d1b1bc2082df17358204dff
Mohammad Vatankhah <mamadvatan80@gmail.com>
2021-03-27 11:12:09 +0430

    player class created

---
+++ src/Player.java
@@ -0,0 +1,114 @@
+abstract class Player {
+    protected String name;
+    protected int numberOfVotes;
+    protected boolean isAlive;
+    protected boolean isMafia;
+    protected boolean isJoker;
+    protected boolean isVillager;
+    protected boolean isDetective;
+    protected boolean isDoctor;
+    protected boolean isBulletproof;
+    protected boolean isGodfather;
+    protected boolean isSilencer;
+    protected boolean savedByDoctor;
+    protected boolean getShot;
+    Player(String name){
+        this.name = name;
+    }
+
+    public int getNumberOfVotes() {
+        return numberOfVotes;
+    }
+
+    public void addNumberOfVotes() {
+        this.numberOfVotes += 1;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
+    public boolean isMafia() {
+        return isMafia;
+    }
+
+    public void setMafia(boolean mafia) {
+        isMafia = mafia;
+    }
+
+    public boolean isJoker() {
+        return isJoker;
+    }
+
+    public void setJoker(boolean joker) {
+        isJoker = joker;
+    }
+
+    public boolean isVillager() {
+        return isVillager;
+    }
+
+    public void setVillager(boolean villager) {
+        isVillager = villager;
+    }
+
+    public boolean isDetective() {
+        return isDetective;
+    }
+
+    public void setDetective(boolean detective) {
+        isDetective = detective;
+    }
+
+    public boolean isDoctor() {
+        return isDoctor;
+    }
+
+    public void setDoctor(boolean doctor) {
+        isDoctor = doctor;
+    }
+
+    public boolean isBulletproof() {
+        return isBulletproof;
+    }
+
+    public void setBulletproof(boolean bulletproof) {
+        isBulletproof = bulletproof;
+    }
+
+    public boolean isGodfather() {
+        return isGodfather;
+    }
+
+    public void setGodfather(boolean godfather) {
+        isGodfather = godfather;
+    }
+
+    public boolean isSilencer() {
+        return isSilencer;
+    }
+
+    public void setSilencer(boolean silencer) {
+        isSilencer = silencer;
+    }
+
+    public boolean isSavedByDoctor() {
+        return savedByDoctor;
+    }
+
+    public void setSavedByDoctor(boolean savedByDoctor) {
+        this.savedByDoctor = savedByDoctor;
+    }
+
+    public boolean isGetShot() {
+        return getShot;
+    }
+
+    public void setGetShot(boolean getShot) {
+        this.getShot = getShot;
+    }
+}
